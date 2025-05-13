f119bd41291e1361e24f34edb607f12188372772
Ignacy Waluzenicz <ignacy.waluzenicz@leancode.pl>
2025-05-13 18:12:35 +0200

    refactor: extract simulation load config and update default entity values

---
+++ web/apps/web/src/services/simulationConfig.ts
@@ -0,0 +1,14 @@
+export const simulationConfig = {
+    loads: [
+        {
+            type: "load",
+            name: "Building1",
+            consumptionRate: 90.0,
+        },
+        {
+            type: "load",
+            name: "Building2",
+            consumptionRate: 30.0,
+        },
+    ],
+}
